0eb262f7e755db00672c8716f07f8a8c4cffa143
Alexandre Oliveira <63254630+AlexandreOliver@users.noreply.github.com>
2025-07-08 23:03:43 +0000

    Adicionado muitas coisas

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import pg from "pg";
+
+async function query(query) {
+  const client = new pg.Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+
+  const result = await client.query(query);
+  client.end();
+
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function ddd(request, response) {
+import database from "/infra/database";
+
+async function index(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
   response.status(200).json({ tex: "aaaaaaaaaa" });
-  console.log("Bateu aqui");
+
+  console.log(result.rows[0]);
 }
 
-export default ddd;
+export default index;
