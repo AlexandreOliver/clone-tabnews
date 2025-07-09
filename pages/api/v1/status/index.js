3360ab47d223de74ed65305eec620164b84614ee
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2025-07-09 13:01:09 -0300

    finish api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
-import database from "/infra/database";
+import database from "/infra/database.js";
 
 async function index(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  response.status(200).json({ tex: "aaaaaaaaaa" });
+  const updateAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
 
-  console.log(result.rows[0]);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseOpeningConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersion = databaseVersionResult.rows[0]["server_version"];
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0]["max_connections"];
+  const databaseOpeningConnections =
+    databaseOpeningConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        connections_: databaseOpeningConnections,
+      },
+    },
+  });
 }
 
 export default index;
